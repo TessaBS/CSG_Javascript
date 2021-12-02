be66cae5e2697107b2ab9b5634e8269add6085a5
TessaBS <153128@csgaugustinus.nl>
2021-12-02 10:00:03 +0000

    x

--- JS/H1/H1O21.js
+++ JS/H1/H1O21.js
@@ -20,17 +20,22 @@ function draw() {
   stroke('white');
   strokeWeight(10);
 
-  if (mouseIsPressed == true && snelheid == 5) {
-      fill('green');
+  if (snelheid == 5) {
+    fill('green');
   }
   else {
     fill('dodgerblue');
   }
   
-  if (x>880) {
-    snelheid = -1*snelheid;
+  if (mouseIsPressed == false){
+    fill('red')
+    diameter=100
   }
-  if (x<120) {
+  else{
+    diameter=200
+  }
+
+  if (x > 880 || x < 120) {
     snelheid = -1*snelheid;
   }
 
