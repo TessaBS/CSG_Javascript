21cf76be6fce2a07f04790e2da98d6847e5562fb
TessaBS <153128@csgaugustinus.nl>
2021-12-24 09:48:19 +0000

    x

--- JS/H2/H2O05.js
+++ JS/H2/H2O05.js
@@ -18,21 +18,19 @@ function setup() {
 }
 
 function draw() {
-  
+  background(brug);
   tekenRaster();
+  image(spriteJos,4*celGrootte,3*celGrootte);
 }
 
 function tekenRaster() {
   push();
   noFill();
   stroke('grey');
-  /*
-  Maak hieronder een dubbele herhaling om een raster te maken.
-  HINT: je kunt terugkijken naar het raster dat je in H1 hebt gemaakt.
-  Maak gebruik van de variabelen die bovenaan zijn gedeclareerd.
-  */
-  
-      rect(4*celGrootte,2*celGrootte,celGrootte,celGrootte);
-
+  for(var rij=0; rij<aantalRijenRaster*celGrootte; rij += celGrootte){
+    for(var kolom=0; kolom<aantalKolommenRaster*celGrootte; kolom+= celGrootte){
+      rect(kolom,rij,celGrootte,celGrootte);
+    }
+  }
   pop();
 }
